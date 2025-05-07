656ae942f5a6fde97baf2cade6629f6ef0a246f2
Merlin <merliin3007@gmail.com>
2025-05-08 01:39:46 +0200

    generate and serve OpenAPI documentation

--- backend/src/Server.hs
+++ backend/src/Server.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Server (runServer, API, app, server) where
 
+import Control.Lens
 import Control.Monad.IO.Class
+import Data.OpenApi (OpenApi, description, info, license, servers, title, version)
 import Data.Vector
 import Database (getConnection)
 import qualified Database.Sessions as Sessions
@@ -16,12 +19,17 @@ import qualified Hasql.Session as Session
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import Servant.OpenApi
 
 type PingAPI = "ping" :> Get '[JSON] String
 
 type UserAPI = "users" :> Get '[JSON] [User.User]
 
 type API = PingAPI :<|> UserAPI
+
+type SwaggerAPI = "swagger.json" :> Get '[JSON] OpenApi
+
+type DocumentedAPI = SwaggerAPI :<|> API
 
 pingServer :: Server PingAPI
 pingServer = return "pong"
@@ -32,14 +40,26 @@ userServer = liftIO $ do
   Right vector <- Session.run Sessions.getUsers connection
   return $ toList vector
 
-server :: Server API
-server = pingServer :<|> userServer
+api :: Proxy API
+api = Proxy
 
-testAPI :: Proxy API
-testAPI = Proxy
+swagger :: OpenApi
+swagger =
+  toOpenApi api
+    & info . title .~ "Fachprüfungsordnung API"
+    & info . version .~ "1.0"
+    & info . description ?~ "This is the API for the Fachprüfungsordnung editor."
+    & info . license ?~ "AGPL3"
+    & servers .~ ["http://batailley.informatik.uni-kiel.de/api/"]
+
+server :: Server DocumentedAPI
+server = return swagger :<|> pingServer :<|> userServer
+
+documentedAPI :: Proxy DocumentedAPI
+documentedAPI = Proxy
 
 app :: Application
-app = serve testAPI server
+app = serve documentedAPI server
 
 runServer :: IO ()
 runServer = run 80 app
