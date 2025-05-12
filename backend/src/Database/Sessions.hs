4f95f4b02522f75c5c743f2ee94fb52ab6a02714
Merlin <merliin3007@gmail.com>
2025-05-12 22:05:09 +0200

    backend: cleanup

--- backend/src/Database/Sessions.hs
+++ backend/src/Database/Sessions.hs
@@ -1,10 +1,14 @@
-module Database.Sessions (getUsers, getUser, putUser, putBlob) where
+module Database.Sessions
+  ( getUsers,
+    getUser,
+    putUser,
+  )
+where
 
 import Data.Text (Text)
 import Data.Vector (Vector)
 import qualified Database.Statements as Statements
 import qualified Database.User as User
-import Database.Utility (compressText, hashText)
 import GHC.Int
 import Hasql.Session (Session, statement)
 
@@ -16,9 +20,3 @@ getUser userEmail = statement userEmail Statements.getUser
 
 putUser :: User.User -> Session Int32
 putUser user = statement user Statements.putUser
-
-putBlob :: Text -> Session Text
-putBlob content = do
-  let hash = hashText content
-  statement (hash, compressText content) Statements.putBlob
-  return hash
