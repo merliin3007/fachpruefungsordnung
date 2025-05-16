merliin3007/fachpruefungsordnung

commit 5e9ab053f42aeeb0144184f20c3ccd17dc88c0f2

backend: Fix inconsistent migrations (#39)
Reviewed-on: https://codeberg.org/fachpruefungsordnung/fachpruefungsordnung/pulls/39
Co-authored-by: Merlin <info@bahn.sh>
Co-committed-by: Merlin <info@bahn.sh>

backend/migrations/001_init.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
@@ -12,13 +12,13 @@ VALUES
 
 CREATE TYPE ROLE AS ENUM ('admin', 'user');
 
-CREATE TABLE classes (
+CREATE TABLE IF NOT EXISTS classes (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE roles (
-    user_id UUID NOT NULL,
+CREATE TABLE IF NOT EXISTS roles (
+    user_id INTEGER NOT NULL,
     class_id INTEGER NOT NULL,
     role ROLE NOT NULL,
     PRIMARY KEY (user_id, class_id),

backend/migrations/002_versioning.up.sql
@@ -1,15 +1,15 @@
-CREATE TABLE nodes (
+CREATE TABLE IF NOT EXISTS nodes (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     kind TEXT NOT NULL
 );
 
-CREATE TABLE node_versions (
+CREATE TABLE IF NOT EXISTS node_versions (
     hash BYTEA PRIMARY KEY NOT NULL, -- hash over content and child hashes
     node INTEGER NOT NULL REFERENCES nodes (id),
     content TEXT
 );
 
-CREATE TABLE trees (
+CREATE TABLE IF NOT EXISTS trees (
     child BYTEA NOT NULL REFERENCES node_versions (hash),
     parent BYTEA NOT NULL REFERENCES node_versions (hash),
     child_position INTEGER NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE trees (
     UNIQUE (parent, child_position)
 );
 
-CREATE TABLE commits (
+CREATE TABLE IF NOT EXISTS commits (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     creation_ts TIMESTAMP NOT NULL DEFAULT NOW (),
     author UUID NOT NULL REFERENCES users (id),
@@ -27,7 +27,7 @@ CREATE TABLE commits (
     parent INTEGER REFERENCES commits (id)
 );
 
-CREATE TABLE documents (
+CREATE TABLE IF NOT EXISTS documents (
     id INTEGER GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
     head INTEGER NOT NULL REFERENCES commits (id)

commit 1c7d2042c67ea8f503c5ef580130e9b7f360a576

backend: fixed broken migrations (#42)
The migrations were broken due to wrong id type introduced by me in #39
This pr fixes restores the correct user_id type of uuid.

Reviewed-on: https://codeberg.org/fachpruefungsordnung/fachpruefungsordnung/pulls/42
Co-authored-by: Merlin <merliin3007@gmail.com>
Co-committed-by: Merlin <merliin3007@gmail.com>

backend/migrations/001_init.up.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS classes (
 );
 
 CREATE TABLE IF NOT EXISTS roles (
-    user_id INTEGER NOT NULL,
+    user_id UUID NOT NULL,
     class_id INTEGER NOT NULL,
     role ROLE NOT NULL,
     PRIMARY KEY (user_id, class_id),