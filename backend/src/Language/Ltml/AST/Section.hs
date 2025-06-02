b4883ab5f1cca3a324c841d7b1521f14d2846a44
Einhard Leichtfuß <alguien@respiranto.de>
2025-06-02 21:20:24 +0200

    backend/lang: Add distinct (section) heading type
    Everything should have a type, s.t. all parsers can be of the form
      xP :: XType -> Parser X
    
    The SectionFormat now no longer includes the HeadingFormat.

--- backend/src/Language/Ltml/AST/Section.hs
+++ backend/src/Language/Ltml/AST/Section.hs
@@ -1,20 +1,26 @@
-module Language.Ltml.AST.Section (Section) where
+module Language.Ltml.AST.Section
+    ( Section (..)
+    , Heading (..)
+    , SectionChild (..)
+    )
+where
 
-import Data.Text (Text)
+import Language.Lsd.AST.Format (HeadingFormat)
 import Language.Lsd.AST.Type.Section (SectionFormat)
 import Language.Ltml.AST.Node (Node)
 import Language.Ltml.AST.Paragraph (Paragraph)
+import Language.Ltml.AST.Text (PlainTextTree)
 
--- sectionKind = Section
--- sectionType = Section SectionFormat
 data Section
     = Section
         SectionFormat
         Heading
         [Node SectionChild]
 
--- TODO: Use PlainText (yet to be defined).
-newtype Heading = Heading Text
+data Heading
+    = Heading
+        HeadingFormat
+        [PlainTextTree]
 
 data SectionChild
     = SectionChildSection Section
