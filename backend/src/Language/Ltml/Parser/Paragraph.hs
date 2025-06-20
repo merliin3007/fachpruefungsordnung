0f13a35e4b1841685b365e04fc49bf264760463b
Einhard Leichtfuß <alguien@respiranto.de>
2025-06-20 18:25:08 +0200

    ltml: Add general labeling parser
    We removed `:` from the set of acceptable label characters.  This is to
    keep parsing simple, and maybe avoid confusing users.
    
    Also,
     - update docs on label and labeling syntax
     - use labeling parser for paragraphs (addresses #135 w.r.t. paragraphs)

--- backend/src/Language/Ltml/Parser/Paragraph.hs
+++ backend/src/Language/Ltml/Parser/Paragraph.hs
@@ -3,13 +3,18 @@ module Language.Ltml.Parser.Paragraph
     )
 where
 
+import Control.Applicative (optional)
 import Language.Lsd.AST.Type.Paragraph (ParagraphType (..))
 import Language.Ltml.AST.Node (Node (..))
 import Language.Ltml.AST.Paragraph (Paragraph (..))
 import Language.Ltml.Parser (Parser)
+import Language.Ltml.Parser.Label (labelingP)
 import Language.Ltml.Parser.Text (textForestP)
+import Text.Megaparsec (try)
+import Text.Megaparsec.Char (char)
 
--- TODO: Parse node label.
+-- TODO: Avoid `try`.
 paragraphP :: ParagraphType -> Parser (Node Paragraph)
-paragraphP (ParagraphType fmt tt) =
-    Node Nothing . Paragraph fmt <$> textForestP tt
+paragraphP (ParagraphType fmt tt) = do
+    label <- optional $ try $ labelingP <* char '\n'
+    Node label . Paragraph fmt <$> textForestP tt
--- docs/language/ltml/document.md
+++ docs/language/ltml/document.md
@@ -4,6 +4,7 @@ An LTML document is composed of a [header](#header) and a [body](#body),
 where the former contains some metadata and the latter is a tree of *nodes*.
 The permitted header nodes, body nodes, and their composability are determined
 by an [LTML Schema Definition](../lsd.md).
+Body nodes can be [labeled](./general/label.md).
 
 Generally, only parts of an LTML document are represented as human-readable
 text; the nodes close to the root are generally only editable via a GUI.  This
