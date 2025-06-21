3fd483768647d5fcc448d167686bf100fa770144
Einhard Leichtfuß <alguien@respiranto.de>
2025-06-21 17:48:04 +0200

    backend/ltml: Add sentence start parsing
    There is still work to do; notably, we currently accept
    * a sentence start token at the end of a paragraph, and
    * a paragraph composed of nothing but a sentence start token.
    
    Closes: #103
    Closes: #152
    
    Opens: #168
    Opens: #169

--- backend/src/Language/Ltml/Parser.hs
+++ backend/src/Language/Ltml/Parser.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Language.Ltml.Parser
     ( Parser
     , MonadParser
+    , ParserWrapper (wrapParser)
     , nli
     , nextIndentLevel
     , checkIndent
@@ -31,6 +33,12 @@ import qualified Text.Megaparsec.Char.Lexer as L (indentLevel)
 type Parser = Parsec Void Text
 
 type MonadParser m = (MonadParsec Void Text m, MonadFail m)
+
+class ParserWrapper m where
+    wrapParser :: Parser a -> m a
+
+instance ParserWrapper Parser where
+    wrapParser = id
 
 nextIndentLevel :: Pos -> Pos
 nextIndentLevel = (<> mkPos 2)
