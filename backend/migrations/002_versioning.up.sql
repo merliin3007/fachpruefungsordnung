merliin3007/fachpruefungsordnung

commit 2ef2802b7218396b63661cd648c5cfcb3772e58a

Backend: Internal Document API and Commits with Multiple Parents (#175)
This PR adds new functions to the internal Version Control API to work
with documents:
- `getDocument` gets a document by id
- `createDocument` creates a new document for a user group
- `createDocumentCommit` creates a new commit and tries to set it as the
new document head commit. This can fail if the new commit is unrelated
to the currenty set head commit of the document. This might change in
the future, as there are other cases in which it might be sensible to
fail the operation.

Also, commits can now have multiple parent commits. (yeah, this probably
should have been a separate PR)

---------

Co-authored-by: Finn Evers <dev@bahn.sh>

backend/migrations/002_versioning.up.sql
@@ -24,12 +24,25 @@ CREATE TABLE IF NOT EXISTS commits (
     author UUID NOT NULL REFERENCES users (id),
     message TEXT NOT NULL,
     root BYTEA NOT NULL REFERENCES node_versions (hash),
-    parent INTEGER REFERENCES commits (id)
+    base INTEGER REFERENCES commits (id),
+    height INTEGER NOT NULL, -- distance to the root commit (directly via base!)
+    root_commit INTEGER REFERENCES commits (id)
+);
+
+CREATE TABLE IF NOT EXISTS commit_trees (
+    parent INTEGER NOT NULL REFERENCES commits (id),
+    child INTEGER NOT NULL REFERENCES commits (id),
+    PRIMARY KEY (parent, child)
+);
+
+CREATE TABLE IF NOT EXISTS commit_base (
+    commit INTEGER PRIMARY KEY NOT NULL REFERENCES commits (id),
+    base INTEGER NOT NULL REFERENCES commits (id) -- parent, if only one exists, lca of parents otherwise
 );
 
 CREATE TABLE IF NOT EXISTS documents (
-    id INTEGER GENERATED ALWAYS AS IDENTITY UNIQUE,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
     name TEXT NOT NULL,
     group_id INTEGER NOT NULL REFERENCES groups (id),
-    head INTEGER NOT NULL REFERENCES commits (id)
+    head INTEGER REFERENCES commits (id)
 );

backend/src/VersionControl/Statements.hs
@@ -7,9 +7,15 @@ module VersionControl.Statements
     , putVersion
     , putEdge
     , createCommit
+    , putCommitRel
+    , getCommitParentIDs
     , getCommit
+    , getCommitNode
     , getChildrenByParentHash
     , getVersion
+    , createDocument
+    , getDocument
+    , setDocumentHead
     )
 where
 
@@ -18,7 +24,9 @@ import Data.Text
 import Data.Vector
 import Hasql.Statement
 import Hasql.TH
+import UserManagement.Group (GroupID)
 import VersionControl.Commit
+import VersionControl.Document (Document (..), DocumentID (..))
 import VersionControl.Hash (Hash (..), Hashed (..))
 import VersionControl.Tree
 
@@ -28,8 +36,12 @@ createNode =
     rmap
         NodeID
         [singletonStatement|
-    insert into nodes (kind) values ($1 :: text) returning id :: int4
-  |]
+            insert into nodes
+                (kind)
+            values
+                ($1 :: text)
+            returning id :: int4
+        |]
 
 -- | statement to get a node kind by the corresponding 'NodeID'
 getNodeKind :: Statement NodeID Text
@@ -37,8 +49,13 @@ getNodeKind =
     lmap
         unNodeID
         [singletonStatement|
-    select kind :: text from nodes where id = $1 :: int4
-  |]
+            select
+                kind :: text
+            from
+                nodes
+            where
+                id = $1 :: int4
+        |]
 
 -- | statement to put a specific version of a document tree into the database
 putVersion :: Statement (Hashed NodeWithRef) ()
@@ -48,10 +65,12 @@ putVersion =
             (hash, ref, nodeContent node)
         )
         [resultlessStatement|
-      insert into node_versions (hash, node, content)
-      values ($1 :: bytea, $2 :: int4, $3 :: text?)
-      on conflict (hash) do nothing
-    |]
+            insert into node_versions
+                (hash, node, content)
+            values
+                ($1 :: bytea, $2 :: int4, $3 :: text?)
+            on conflict (hash) do nothing
+        |]
 
 -- | statement to obtain a specific version of a document tree by its hash
 getVersion :: Statement Hash (Hashed NodeWithRef)
@@ -63,17 +82,17 @@ getVersion =
         $ lmap
             (\(Hash bs) -> bs)
             [singletonStatement|
-      select
-        hash :: bytea,
-        node :: int4,
-        kind :: text,
-        content :: text?
-      from
-        node_versions
-        join nodes on node = id
-      where
-        hash = $1 :: bytea
-    |]
+                select
+                    hash :: bytea,
+                    node :: int4,
+                    kind :: text,
+                    content :: text?
+                from
+                    node_versions
+                    join nodes on node = id
+                where
+                    hash = $1 :: bytea
+            |]
 
 -- | statement to put an edge of document node version into the database
 putEdge :: Statement DataEdge ()
@@ -83,9 +102,11 @@ putEdge =
             (unHash parent, unHash child, position, title)
         )
         [resultlessStatement|
-      insert into trees (parent, child, child_position, child_title)
-      values ($1 :: bytea, $2 :: bytea, $3 :: int4, $4 :: text)
-    |]
+            insert into trees
+                (parent, child, child_position, child_title)
+            values
+                ($1 :: bytea, $2 :: bytea, $3 :: int4, $4 :: text)
+        |]
 
 -- | statement to obtain all child document node version by their parent's hash
 getChildrenByParentHash :: Statement Hash (Vector (Text, Hashed NodeWithRef))
@@ -99,63 +120,188 @@ getChildrenByParentHash =
         $ lmap
             (\(Hash bs) -> bs)
             [vectorStatement|
-    select
-      child_title :: text,
-      hash :: bytea,
-      node :: int4,
-      kind :: text,
-      content :: text?
-    from
-      node_versions
-      join trees on hash = child
-      join nodes on node = id
-    where
-      parent = $1 :: bytea
-    order by child_position asc
-  |]
+                select
+                    child_title :: text,
+                    hash :: bytea,
+                    node :: int4,
+                    kind :: text,
+                    content :: text?
+                from
+                    node_versions
+                    join trees on hash = child
+                    join nodes on node = id
+                where
+                    parent = $1 :: bytea
+                order by child_position asc
+            |]
 
 -- | statement to create a commit
 createCommit :: Statement CommitBody CommitHeader
 createCommit =
     rmap (\(newID, ts) -> CommitHeader (CommitID newID) ts) $
         lmap
-            ( \(CommitBody info root) ->
+            ( \(CommitBody info root base) ->
                 ( commitInfoAuthor info
                 , commitInfoMessage info
                 , unHash $ treeRefHash root
-                , unCommitID . commitRefID <$> commitInfoParent info
+                , unCommitID <$> base
                 )
             )
             [singletonStatement|
-      insert into commits (author, message, root, parent)
-      values ($1 :: uuid, $2 :: text?, $3 :: bytea, $4 :: int4?)
-      returning id :: int4, creation_ts :: timestamp
-    |]
+                insert into commits
+                    (author, message, root, base, height)
+                values (
+                    $1 :: uuid,
+                    $2 :: text?,
+                    $3 :: bytea,
+                    $4 :: int4?,
+                    coalesce((
+                        select height + 1
+                        from commits
+                        where id = $4 :: int4?
+                    ), 0),
+                    coalesce((
+                        select root_commit
+                        from commits
+                        where id = $4 :: int4?
+                    ), $4 :: int4?)
+                )
+                returning
+                    id :: int4, creation_ts :: timestamp
+            |]
+
+-- | register a parent-child relation between two commits
+putCommitRel :: Statement CommitRel ()
+putCommitRel =
+    lmap
+        (\(CommitRel parent child) -> (unCommitID parent, unCommitID child))
+        [resultlessStatement|
+            insert into
+                commit_trees (parent, child)
+            values
+                ($1 :: int4, $2 :: int4)
+        |]
+
+-- | get the ids of all parents of a commit
+getCommitParentIDs :: Statement CommitID (Vector CommitID)
+getCommitParentIDs =
+    rmap (fmap CommitID) $
+        lmap
+            unCommitID
+            [vectorStatement|
+                select
+                    parent :: int4
+                from
+                    commit_trees
+                where
+                    child = $1 :: int4
+            |]
 
 -- | statement to get a commit by its 'CommitID'
-getCommit :: Statement CommitID ExistingCommit
+getCommit :: Statement CommitID ([CommitID] -> ExistingCommit)
 getCommit =
     rmap
-        ( \(ref, ts, author, message, root, parent) ->
+        ( \(ref, ts, author, message, root, base) parents ->
             ExistingCommit
                 (CommitHeader (CommitID ref) ts)
                 ( CommitBody
-                    (CommitInfo author message (Ref . CommitID <$> parent))
+                    (CommitInfo author message parents)
                     (Ref (Hash root))
+                    (CommitID <$> base)
                 )
         )
         $ lmap
             (\(CommitID i) -> i)
             [singletonStatement|
-        select
-          id :: int4,
-          creation_ts :: timestamp,
-          author :: uuid,
-          message :: text?,
-          root :: bytea,
-          parent :: int4?
-        from
-          commits
-        where
-          id = $1 :: int4
-    |]
+                select
+                id :: int4,
+                creation_ts :: timestamp,
+                author :: uuid,
+                message :: text?,
+                root :: bytea,
+                base :: int4?
+                from
+                commits
+                where
+                id = $1 :: int4
+            |]
+
+-- | get commit node by commit id.
+-- contains metadata about a commits position in the commit graph.
+getCommitNode :: Statement CommitID CommitNode
+getCommitNode =
+    rmap
+        ( \(commit, base, height, root) ->
+            CommitNode
+                (CommitID commit)
+                (CommitID <$> base)
+                height
+                (CommitID <$> root)
+        )
+        $ lmap
+            unCommitID
+            [singletonStatement|
+                select
+                    id :: int4,
+                    base :: int4?,
+                    height :: int4,
+                    root_commit :: int4?
+                from
+                    commits
+                where
+                    id = $1 :: int4
+            |]
+
+-- | statement to create a node of a certain kind
+createDocument :: Statement (Text, GroupID) DocumentID
+createDocument =
+    rmap
+        DocumentID
+        [singletonStatement|
+            insert into documents
+                (name, group_id)
+            values
+                ($1 :: text),
+                ($2 :: int4)
+            returning id :: int4
+        |]
+
+-- | statement to get a document by its corresponding 'DocumentID'
+getDocument :: Statement DocumentID Document
+getDocument =
+    rmap
+        ( \(document, name, groupID, headCommit) ->
+            Document
+                (DocumentID document)
+                name
+                groupID
+                (CommitID <$> headCommit)
+        )
+        $ lmap
+            unDocumentID
+            [singletonStatement|
+            select
+                id :: int4,
+                name :: text,
+                group_id :: int4,
+                head :: int4?
+            from
+                documents
+            where
+                id = $1 :: int4
+        |]
+
+-- | statement to put a specific version of a document tree into the database
+setDocumentHead :: Statement (DocumentID, CommitID) ()
+setDocumentHead =
+    lmap
+        ( \(DocumentID documentId, CommitID commitId) ->
+            (documentId, commitId)
+        )
+        [resultlessStatement|
+            update documents
+            set
+                head = $2 :: int4
+            where
+                id = $1 :: int4
+        |]

backend/package.yaml
@@ -26,6 +26,7 @@ dependencies:
   - tuple
   - lens
   - profunctors
+  - containers
   - hasql
   - hasql-th
   - hasql-pool

backend/src/Lib.hs
@@ -52,12 +52,21 @@ testTree =
 testCommits :: Connection -> IO ExistingCommit
 testCommits conn = do
     Right userId <- getUserID "test@test.com" conn
-    let commit =
+    let commit1 =
             CreateCommit
-                (CommitInfo userId (Just "Test Commit") Nothing)
+                (CommitInfo userId (Just "Test Commit") [])
                 (Value testTree)
-    Right newCommit <- VC.createCommit commit $ VC.Context conn
-    return newCommit
+    Right newCommit <- VC.createCommit commit1 $ VC.Context conn
+    let commit2 =
+            CreateCommit
+                ( CommitInfo
+                    userId
+                    (Just "Another Commit")
+                    [commitHeaderID (existingCommitHeader newCommit)]
+                )
+                (Value testTree)
+    Right newCommit2 <- VC.createCommit commit2 $ VC.Context conn
+    return newCommit2
   where
     getUserID email = Session.run (statement email UStatements.getUserID)
 

backend/src/VersionControl.hs
@@ -2,15 +2,24 @@ module VersionControl
     ( Context (..)
     , createCommit
     , getCommit
+    , getDocument
+    , createDocument
+    , createDocumentCommit
     ) where
 
 import Control.Arrow (left)
 import Data.Functor ((<&>))
+import Data.Text (Text)
 import Hasql.Connection (Connection)
 import Hasql.Session (Session, SessionError)
 import qualified Hasql.Session as Session
+import UserManagement.Group (GroupID)
 import VersionControl.Commit (CommitID, CreateCommit, ExistingCommit)
-import VersionControl.Error (VersionControlError (..))
+import VersionControl.Document (Document, DocumentID)
+import VersionControl.Error
+    ( VersionControlError (..)
+    , flattenVersionControlError
+    )
 import qualified VersionControl.Sessions as Sessions
 
 -- | context for the version control (currently just a database connection)
@@ -18,12 +27,40 @@ newtype Context = Context
     { unConnection :: Connection
     }
 
+-- | get a document by id
+getDocument :: DocumentID -> Context -> IO (Either VersionControlError Document)
+getDocument = runSession . Sessions.getDocument
+
+-- | creates a new document in the specified user group
+createDocument
+    :: Text
+    -- ^ The name of the document
+    -> GroupID
+    -- ^ The group which owns the document
+    -> Context
+    -> IO (Either VersionControlError DocumentID)
+createDocument = (runSession .) . Sessions.createDocument
+
 -- | creates a commit in the given version control context
 createCommit
     :: CreateCommit
     -> Context
     -> IO (Either VersionControlError ExistingCommit)
 createCommit = runSession . Sessions.createCommit
+
+-- | creates a commit in the specified document
+--   the newly created commit must be related to the current head commit of the
+--   document (if any).
+createDocumentCommit
+    :: DocumentID
+    -> CreateCommit
+    -> Context
+    -> IO (Either VersionControlError Document)
+createDocumentCommit documentID commit ctx =
+    flattenVersionControlError
+        <$> createDocumentCommit' documentID commit ctx
+  where
+    createDocumentCommit' = (runSession .) . Sessions.createDocumentCommit
 
 -- | gets a commit from the given version control context
 getCommit

backend/src/VersionControl/Commit.hs
@@ -9,6 +9,8 @@ module VersionControl.Commit
     , CommitBody (..)
     , CommitID (..)
     , CommitRef
+    , CommitRel (..)
+    , CommitNode (..)
     , commitRefID
     , commitMapRoot
     )
@@ -40,7 +42,7 @@ import VersionControl.Tree
 newtype CommitID = CommitID
     { unCommitID :: Int32
     }
-    deriving (Show, Generic)
+    deriving (Show, Generic, Eq, Ord)
 
 instance ToJSON CommitID where
     toJSON = toJSON . unCommitID
@@ -62,8 +64,9 @@ commitRefID (Value (ExistingCommit header _)) = commitHeaderID header
 -- | contains all information needed to create a new commit
 data CreateCommit
     = CreateCommit
-        CommitInfo
-        (TreeRef NodeWithMaybeRef)
+    { createCommitInfo :: CommitInfo
+    , createCommitRoot :: TreeRef NodeWithMaybeRef
+    }
     deriving (Show)
 
 instance ToJSON CreateCommit where
@@ -94,8 +97,9 @@ instance ToSchema CreateCommit where
 -- | represents a commit guaranteed to exist in the database
 data ExistingCommit
     = ExistingCommit
-        CommitHeader
-        CommitBody
+    { existingCommitHeader :: CommitHeader
+    , existingCommitBody :: CommitBody
+    }
     deriving (Show)
 
 instance ToJSON ExistingCommit where
@@ -130,25 +134,27 @@ commitMapRoot
     :: (TreeRef (Hashed NodeWithRef) -> TreeRef (Hashed NodeWithRef))
     -> ExistingCommit
     -> ExistingCommit
-commitMapRoot f (ExistingCommit header (CommitBody info root)) =
-    ExistingCommit header (CommitBody info (f root))
+commitMapRoot f (ExistingCommit header (CommitBody info root base)) =
+    ExistingCommit header (CommitBody info (f root) base)
 
 -- | contains the content of an existing commit
 data CommitBody
     = CommitBody
         CommitInfo
         (TreeRef (Hashed NodeWithRef))
+        (Maybe CommitID)
     deriving (Show)
 
 instance ToJSON CommitBody where
-    toJSON (CommitBody info root) =
-        Aeson.object ["info" .= info, "root" .= root]
+    toJSON (CommitBody info root base) =
+        Aeson.object ["info" .= info, "root" .= root, "base" .= base]
 
 instance FromJSON CommitBody where
     parseJSON = Aeson.withObject "CommitBody" $ \v ->
         CommitBody
             <$> v .: "info"
             <*> v .: "root"
+            <*> v .: "base"
 
 instance ToSchema CommitBody where
     declareNamedSchema _ = do
@@ -203,16 +209,16 @@ instance ToSchema CommitHeader where
 data CommitInfo = CommitInfo
     { commitInfoAuthor :: UUID
     , commitInfoMessage :: Maybe Text
-    , commitInfoParent :: Maybe CommitRef
+    , commitInfoParents :: [CommitID]
     }
     deriving (Show, Generic)
 
 instance ToJSON CommitInfo where
-    toJSON (CommitInfo author message parent) =
+    toJSON (CommitInfo author message parents) =
         Aeson.object
             [ "author" .= author
             , "message" .= message
-            , "parent" .= parent
+            , "parents" .= parents
             ]
 
 instance FromJSON CommitInfo where
@@ -220,7 +226,7 @@ instance FromJSON CommitInfo where
         CommitInfo
             <$> v .: "author"
             <*> v .: "message"
-            <*> v .: "parent"
+            <*> v .: "parents"
 
 instance ToSchema CommitInfo where
     declareNamedSchema _ = do
@@ -238,3 +244,17 @@ instance ToSchema CommitInfo where
                             , ("parent", parentSchema)
                             ]
                     & required .~ ["author"]
+
+-- | describes a parent-child relation between two commits
+data CommitRel = CommitRel
+    { commitRelParent :: CommitID
+    , commitRelChild :: CommitID
+    }
+
+-- | information about a commits position in the commit graph.
+data CommitNode = CommitNode
+    { commitNodeID :: CommitID
+    , commitNodeBase :: Maybe CommitID
+    , commitNodeHeight :: Int32
+    , commitNodeRootCommit :: Maybe CommitID
+    }

backend/src/VersionControl/Document.hs
@@ -1,0 +1,24 @@
+module VersionControl.Document
+    ( Document (..)
+    , DocumentID (..)
+    , withNewDocumentHead
+    ) where
+
+import Data.Text (Text)
+import GHC.Int (Int32)
+import UserManagement.Group (GroupID)
+import VersionControl.Commit (CommitID)
+
+newtype DocumentID = DocumentID
+    {unDocumentID :: Int32}
+
+data Document = Document
+    { documentID :: DocumentID
+    , documentName :: Text
+    , groupId :: GroupID
+    , documentHead :: Maybe CommitID
+    }
+
+-- | Update the document head for the given document
+withNewDocumentHead :: Document -> CommitID -> Document
+withNewDocumentHead doc c = doc {documentHead = Just c}

backend/src/VersionControl/Error.hs
@@ -1,7 +1,30 @@
-module VersionControl.Error (VersionControlError (..)) where
+module VersionControl.Error
+    ( VersionControlError (..)
+    , DocumentError (..)
+    , flattenVersionControlError
+    ) where
 
 -- | represents an error occured during a version control operation
 data VersionControlError
     = InsufficientPrevilige
     | DatabaseError String
+    | DocumentError DocumentError
     deriving (Eq, Show)
+
+-- | represents an error occured during an operation on documents
+data DocumentError = DocumentNewHeadCommitUnrelated
+    deriving (Eq, Show)
+
+class ToVersionControlError a where
+    toVersionControlError :: a -> VersionControlError
+
+instance ToVersionControlError DocumentError where
+    toVersionControlError = DocumentError
+
+flattenVersionControlError
+    :: (ToVersionControlError a)
+    => Either VersionControlError (Either a b)
+    -> Either VersionControlError b
+flattenVersionControlError (Left e) = Left e
+flattenVersionControlError (Right (Left e)) = Left $ toVersionControlError e
+flattenVersionControlError (Right (Right x)) = Right x

commit 5fba5677d962ce930b2a46a6242d9bd13ed1ee0f

Fix Backend Bug (#183)
This PR fixes a bug introduced by me in #175. Just a small typo, forgot
on column in a SQL query. Due to this bug, the server was unable to
start.

Co-authored-by: Finn Evers <dev@bahn.sh>

backend/migrations/002_versioning.up.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS commit_base (
 );
 
 CREATE TABLE IF NOT EXISTS documents (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
     group_id INTEGER NOT NULL REFERENCES groups (id),
     head INTEGER REFERENCES commits (id)

backend/src/VersionControl/Statements.hs
@@ -150,7 +150,7 @@ createCommit =
             )
             [singletonStatement|
                 insert into commits
-                    (author, message, root, base, height)
+                    (author, message, root, base, height, root_commit)
                 values (
                     $1 :: uuid,
                     $2 :: text?,