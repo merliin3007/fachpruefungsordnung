ada88b260c1e7b1522df4084411c972922ad818f
Merlin <info@bahn.sh>
2025-06-22 16:50:49 +0200

    Fix Backend Bug (#183)
    This PR fixes a bug introduced by me in #175. Just a small typo, forgot
    on column in a SQL query. Due to this bug, the server was unable to
    start.
    
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/VersionControl/Statements.hs
+++ backend/src/VersionControl/Statements.hs
@@ -150,7 +150,7 @@ createCommit =
             )
             [singletonStatement|
                 insert into commits
-                    (author, message, root, base, height)
+                    (author, message, root, base, height, root_commit)
                 values (
                     $1 :: uuid,
                     $2 :: text?,
