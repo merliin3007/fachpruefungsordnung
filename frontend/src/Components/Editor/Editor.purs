fac6ce34e39843a878617b032d3b3650fac6adeb
Nick Schmahl <nick.schmahl@wps.de>
2025-06-23 18:48:07 +0200

    resolve merge conflicts

--- frontend/src/Components/Editor/Editor.purs
+++ frontend/src/Components/Editor/Editor.purs
@@ -3,7 +3,6 @@ module FPO.Components.Editor
   , Output(..)
   , Query(..)
   , State
-  , TOCEntry
   , _pdfSlideBar
   , addAnnotation
   , addChangeListener
@@ -29,7 +28,6 @@ import Data.String as String
 import Data.Traversable (for, traverse)
 import Effect (Effect)
 import Effect.Class (class MonadEffect)
-import Effect.Class.Console (log)
 import FPO.Data.Store as Store
 import FPO.Translations.Translator (FPOTranslator, fromFpoTranslator)
 import FPO.Translations.Util (FPOState, selectTranslator)
@@ -429,37 +427,6 @@ addAnnotation annotation session = do
   anns <- Session.getAnnotations session
   Session.setAnnotations (annotation : anns) session
 
--- | Surrounds the selected text with the given left and right strings
--- | and positions the cursor after the inserted left text.
-surroundSelection :: String -> String -> Types.Editor -> Effect Unit
-surroundSelection left right ed = do
-  session <- Editor.getSession ed
-  selection <- Editor.getSelection ed
-  range <- Selection.getRange selection
-  selectedText <- Session.getTextRange range session
-
-  -- Get the start position
-  startPos <- Range.getStart range
-
-  -- Insert the surrounded text
-  let newText = left <> selectedText <> right
-  Session.replace range newText session
-
-  -- Calculate new cursor position (after the left part)
-  let newColumn = (Types.getColumn startPos) + (String.length left)
-
-  -- Move cursor to new position
-  Editor.moveCursorTo (Types.getRow startPos) (Just newColumn) Nothing ed
-  -- Create a new range that encompasses the entire surrounded text
-  newRange <- Range.create
-    (Types.getRow startPos)
-    ((Types.getColumn startPos) + (String.length left))
-    (Types.getRow startPos)
-    ((Types.getColumn startPos) + (String.length newText) - (String.length right))
-
-  -- Set the selection to this new range
-  Selection.setSelectionRange newRange selection
-
 -- Multiple marker removal functions
 -- These functions remove markers by IDs, range, position, or row/column.
 
@@ -480,13 +447,6 @@ removeMarkerByIDs targetIDs markers session = do
   Session.setAnnotations annotations session
 
   pure remainingMarkers
-
-removeMarkerByID
-  :: Int
-  -> Array AnnotatedMarker
-  -> Types.EditSession
-  -> Effect (Array AnnotatedMarker)
-removeMarkerByID targetID = removeMarkerByIDs [ targetID ]
 
 removeMarkerByRange
   :: Types.Range
@@ -509,44 +469,3 @@ removeMarkerByPosition targetPos marker session = do
     col = Types.getColumn targetPos
   targetRange <- Range.create row col row col
   removeMarkerByRange targetRange marker session
-
-removeMarkerByRowCol
-  :: Int
-  -> Int
-  -> Array AnnotatedMarker
-  -> Types.EditSession
-  -> Effect (Array AnnotatedMarker)
-removeMarkerByRowCol row col marker session = do
-  targetRange <- Range.create row col row col
-  removeMarkerByRange targetRange marker session
-
-keyBinding :: Types.Editor -> Event -> Effect Unit
-keyBinding editor_ event = do
-  let keyboardEvent = fromEvent event :: Maybe KeyboardEvent
-  case keyboardEvent of
-    Nothing -> pure unit
-    Just keyEvent -> do
-      let pressedKey = key keyEvent
-      let ctrlKeyPressed = ctrlKey keyEvent
-      if ctrlKeyPressed then
-        case pressedKey of
-          "b" -> makeBold editor_
-          "i" -> makeItalic editor_
-          "u" -> underscore editor_
-          _ -> pure unit
-      else
-        pure unit
-      case pressedKey of
-        "Enter" -> log "Enter" -- Placeholder for Enter key action
-        "Escape" -> log "Escape" -- Placeholder for Escape key action
-        _ -> pure unit
-  pure unit
-
-makeBold :: Types.Editor -> Effect Unit
-makeBold editor_ = surroundSelection "<*" ">" editor_
-
-makeItalic :: Types.Editor -> Effect Unit
-makeItalic editor_ = surroundSelection "</" ">" editor_
-
-underscore :: Types.Editor -> Effect Unit
-underscore editor_ = surroundSelection "<_" ">" editor_
