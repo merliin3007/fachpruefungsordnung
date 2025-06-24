313b90aede4ced82c278bcbfc492b8ac945c8ed1
Nick Schmahl <nick.schmahl@wps.de>
2025-06-24 11:52:24 +0200

    Labels and translations for undo/ redo buttons

--- frontend/src/Translations/Components/Editor.purs
+++ frontend/src/Translations/Components/Editor.purs
@@ -6,9 +6,11 @@ import Simple.I18n.Translation (Translation, fromRecord)
 type EditorLabels =
   ( "editor_fontSizeDown"
       ::: "editor_fontSizeUp"
+      ::: "editor_redo"
       ::: "editor_textBold"
       ::: "editor_textItalic"
       ::: "editor_textUnderline"
+      ::: "editor_undo"
       ::: SNil
   )
 
@@ -16,16 +18,20 @@ enEditor :: Translation EditorLabels
 enEditor = fromRecord
   { editor_fontSizeDown: "Font size up"
   , editor_fontSizeUp: "Font size down"
+  , editor_redo: "Redo (Ctrl+Shift+Z)"
   , editor_textBold: "Bold text (Ctrl+B)"
   , editor_textItalic: "Italic text (Ctrl+I)"
   , editor_textUnderline: "Underline text"
+  , editor_undo: "Undo (Ctrl+Z)"
   }
 
 deEditor :: Translation EditorLabels
 deEditor = fromRecord
   { editor_fontSizeDown: "Schrift vergößern"
   , editor_fontSizeUp: "Schrift verkleinern"
+  , editor_redo: "Vor (Strg+Umschalt+Z)"
   , editor_textBold: "Text fett (Strg+B)"
   , editor_textItalic: "Text kursiv (Strg+I)"
   , editor_textUnderline: "Text unterstreichen"
+  , editor_undo: "Zurück (Strg+Z)"
   }
