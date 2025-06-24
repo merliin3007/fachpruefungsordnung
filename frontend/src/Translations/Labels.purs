c63bb72df6774e2f477a6a721a7d515bbeacb425
Nick Schmahl <nick.schmahl@wps.de>
2025-06-24 12:07:37 +0200

    Good labels and translations for comment buttons

--- frontend/src/Translations/Labels.purs
+++ frontend/src/Translations/Labels.purs
@@ -58,6 +58,8 @@ type Labels =
       ::: "common_submit"
 
       -- | Editor Page
+      ::: "editor_comment"
+      ::: "editor_deleteComment"
       ::: "editor_fontSizeDown"
       ::: "editor_fontSizeUp"
       ::: "editor_redo"
