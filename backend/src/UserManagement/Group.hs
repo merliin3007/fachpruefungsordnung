01bd4a6fa9a8c100ea4bf865d757158b25c1cd5f
Fab1000 <60816643+Fab1000@users.noreply.github.com>
2025-06-25 15:39:29 +0200

    added GET /groups (#204)
    Closes #203
    
    The new endpoint `GET /groups` returns an overview of all existing
    groups as a list of group IDs and group names.

--- backend/src/UserManagement/Group.hs
+++ backend/src/UserManagement/Group.hs
@@ -1,20 +1,30 @@
 {-# LANGUAGE DeriveGeneric #-}
 
-module UserManagement.Group (Group (..), GroupID) where
+module UserManagement.Group (GroupCreate (..), GroupID, GroupOverview (..)) where
 
-import Data.Aeson (FromJSON)
+import Data.Aeson (FromJSON, ToJSON)
 import Data.OpenApi (ToSchema)
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import GHC.Int (Int32)
 
-data Group = Group
-    { groupName :: Text
-    , groupDescription :: Maybe Text
+-- | List of GroupIds and GroupName pairs
+data GroupOverview = GroupOverview
+    { groupID :: GroupID
+    , groupName :: Text
+    }
+    deriving (Generic)
+
+instance ToSchema GroupOverview
+instance ToJSON GroupOverview
+
+data GroupCreate = GroupCreate
+    { groupCreateName :: Text
+    , groupCreateDescription :: Maybe Text
     }
     deriving (Eq, Generic)
 
-instance FromJSON Group
-instance ToSchema Group
+instance FromJSON GroupCreate
+instance ToSchema GroupCreate
 
 type GroupID = Int32
