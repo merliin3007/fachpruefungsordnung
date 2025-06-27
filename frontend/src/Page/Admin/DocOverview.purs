59db47b9f7da466d071dbed0704dbd3eac51940c
Lennart Thorben Dreyer <stu236751@mail.uni-kiel.de>
2025-06-27 22:46:04 +0200

    still not functional, expanded DocOverview file and started moving Modal code into separate file.

--- frontend/src/Page/Admin/DocOverview.purs
+++ frontend/src/Page/Admin/DocOverview.purs
@@ -9,12 +9,21 @@ import Data.Array (filter, length, replicate, slice, (:))
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String (contains)
 import Data.String.Pattern (Pattern(..))
+import Data.Time.Duration
+  ( class Duration
+  , Days(..)
+  , Hours(..)
+  , Seconds(..)
+  , negateDuration
+  , toDuration
+  )
 import Effect.Aff.Class (class MonadAff)
 import FPO.Components.Pagination as P
 import FPO.Data.Navigate (class Navigate, navigate)
 import FPO.Data.Request (getUser)
 import FPO.Data.Route (Route(..))
 import FPO.Data.Store as Store
+import FPO.Page.Home (adjustDateTime)
 import FPO.Page.HTML (addButton, addCard, addColumn, emptyEntryGen)
 import FPO.Translations.Translator (FPOTranslator, fromFpoTranslator)
 import FPO.Translations.Util (FPOState, selectTranslator)
@@ -35,9 +44,18 @@ type Slots =
   ( pagination :: H.Slot P.Query P.Output Unit
   )
 
-type Group = String
+type GroupID = Int
 
-type Document = 
+-- preliminary data type. So far everything seems to use different data for documents, this should be changed.
+type Document =
+    { body ::
+      { name :: String
+      , text :: String}
+    , header ::
+      { creationTs :: DateTime
+      , id :: Int
+      }
+    }
 
 data Action
   = Initialize
@@ -56,11 +74,91 @@ data Action
 type State = FPOState
   ( error :: Maybe String
   , page :: Int
-  , group :: Group
-  , documents :: Array Document 
-  , filteredGroups :: Array Group
-  , groupNameCreate :: String
-  , groupNameFilter :: String
-  -- | This is used to store the group name for deletion confirmation.
+  , groupID :: GroupID
+  , documents :: Array Document
+  , filteredDocuments :: Array Doucment
+  , currentTime :: Maybe DateTime
+  , documentNameFilter :: String
+  -- | This is used to store the document name for deletion confirmation.
   , requestDelete :: Maybe String
   )
+
+  -- | Admin panel page component.
+component
+  :: forall query output m
+   . MonadStore Store.Action Store.Store m
+  => MonadAff m
+  => Navigate m
+  => H.Component query Unit output m
+component =
+  connect selectTranslator $ H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just Initialize
+        , receive = Just <<< Receive
+        }
+    }
+  where
+  initialState :: Connected FPOTranslator Unit -> State
+  initialState { context } =
+    { translator: fromFpoTranslator context
+    , page: 0
+    -- this is only a placeholder, to be changed once connection to backend is established
+    , groupID: 1
+    , documents: []
+    , documentNameFilter: ""
+    , filteredDocuments: []
+    , error: Nothing
+    , requestDelete: Nothing
+    , currentTime: Nothing
+    }
+
+  render :: State -> H.ComponentHTML Action Slots m
+  render state =
+    HH.div
+      [ HP.classes [ HB.row, HB.justifyContentCenter, HB.my5 ] ]
+      $
+        ( case state.requestDelete of
+            Just documentName -> [ deleteConfirmationModal documentName ]
+            Nothing -> []
+        )
+
+  handleAction :: Action -> H.HalogenM State Action Slots output m Unit
+  handleAction = case _ of
+    Initialize -> do
+      u <- liftAff $ getUser
+      when (fromMaybe true (not <$> _.isAdmin <$> u)) $
+        navigate Page404
+      now <- liftEffect nowDateTime
+      H.modify_ _
+        { documents = mockDocuments now
+        , currentTime = Just now
+        }
+
+  mockDocuments :: DateTime -> Array Document
+  mockDocuments now =
+    [
+      { body:
+        { name: "Doc1"
+        , text: "This is the first Document"
+        }
+      , header:
+        {
+          creationTs: now
+          id: 1
+        }
+      }
+      ,
+      { body:
+        { name: "Doc2"
+        , text: "This is the second Document"
+        }
+      , header:
+        {
+          creationTs: adjustDateTime (Days (5.0)) now
+          id: 2
+        }
+      }
+    ]
