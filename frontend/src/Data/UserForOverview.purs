1fdde48d2259e90d86f0eb21be3b360166ed89ea
Nick Schmahl <nick.schmahl@wps.de>
2025-06-28 19:25:07 +0200

    User Page fetches all users from the backend now

---
+++ frontend/src/Data/UserForOverview.purs
@@ -0,0 +1,18 @@
+module Data.UserForOverview where
+
+import Data.Argonaut (class DecodeJson, class EncodeJson)
+import Data.Newtype (class Newtype)
+
+newtype UserForOverview = UserForOverview
+  { userEmail :: String
+  , userID :: String
+  , userName :: String
+  }
+
+getName :: UserForOverview -> String
+getName (UserForOverview { userName }) = userName
+
+derive instance newtypeAppUser :: Newtype UserForOverview _
+
+derive newtype instance encodeJsonAppUser :: EncodeJson UserForOverview
+derive newtype instance decodeJsonAppUser :: DecodeJson UserForOverview
