merliin3007/fachpruefungsordnung

commit b15353df769ee9ec3c83b039e230b390675c0973

Fix Docker for Windows (#226)
See
-
https://wetalk.informatik.uni-kiel.de/fpo-editor/pl/1utkwhsx7trjjpips989owugfo
-
https://wetalk.informatik.uni-kiel.de/fpo-editor/pl/j73foi8ca3rg8mpnb8gsuaatka
-
https://wetalk.informatik.uni-kiel.de/fpo-editor/pl/7hhi9d97b3y8icugu9qnx1yity

frontend/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/sh
 set -eu
 
 TEMPLATE=/etc/nginx/templates/ssl.conf.template

commit 6c9f15ac79dcc351b121e6ab7114547ee8cb9409

Fix Fix for Windows Docker (#228)
Fixes typo introduced by #226

frontend/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 set -eu
 
 TEMPLATE=/etc/nginx/templates/ssl.conf.template