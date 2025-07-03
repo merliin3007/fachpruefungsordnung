08c6214303939779463888eac76fd6220f52e8cc
Einhard Leichtfuß <alguien@respiranto.de>
2025-07-03 23:26:14 +0200

    backend/ltml: Fix greedy paragraph parser
    The paragraph parser previously would also consume what should actually
    be subsequent sections.
    
    It now gets passed a parser to check for the start of a new section.
    
    This necessitated quite some work on the SimpleRegex (we need to know
    all possible successor section types).  We are now actually
    approximately halfway to a full (simple, DFA-style) regex parser.  Thus,
    we might reconsider limiting the regexes to "simple regexes".
    
    fixes: #222

--- backend/src/Language/Ltml/Parser/Section.hs
+++ backend/src/Language/Ltml/Parser/Section.hs
@@ -3,9 +3,13 @@ module Language.Ltml.Parser.Section
     )
 where
 
+import Control.Alternative.Utils (whenAlt)
+import Control.Applicative ((<|>))
+import Control.Monad (void)
 import Data.Bitraversable (bitraverse)
 import Language.Lsd.AST.Common (Keyword)
 import Language.Lsd.AST.SimpleRegex (SimpleRegex)
+import Language.Lsd.AST.SimpleRegex.Utils (Heads (Heads))
 import Language.Lsd.AST.Type.Paragraph (ParagraphType)
 import Language.Lsd.AST.Type.Section
     ( HeadingType (HeadingType)
@@ -21,21 +25,31 @@ import Language.Ltml.AST.Section
 import Language.Ltml.Parser (Parser, nonIndented)
 import Language.Ltml.Parser.Common.Lexeme (nLexeme)
 import Language.Ltml.Parser.Common.SimpleRegex (simpleRegexP)
+import Language.Ltml.Parser.Keyword (keywordP)
 import Language.Ltml.Parser.Paragraph (paragraphP)
 import Language.Ltml.Parser.Text (hangingTextP')
-import Text.Megaparsec (many)
+import Text.Megaparsec (choice, many)
 
-sectionP :: SectionType -> Parser (Node Section)
-sectionP (SectionType kw headingT fmt childrenT) = do
+sectionP :: SectionType -> Parser () -> Parser (Node Section)
+sectionP (SectionType kw headingT fmt childrenT) succStartP = do
     (mLabel, heading) <- nonIndented $ headingP kw headingT
     Node mLabel . Section fmt heading
         <$> nonIndented (bitraverse parsP secsP childrenT)
   where
     parsP :: ParagraphType -> Parser [Node Paragraph]
-    parsP t = many $ nLexeme $ paragraphP t
+    parsP t = many $ nLexeme $ paragraphP t succStartP
 
     secsP :: SimpleRegex SectionType -> Parser [Node Section]
-    secsP = simpleRegexP sectionP
+    secsP = simpleRegexP sectionP'
+      where
+        sectionP' t succs = sectionP t (f succs)
+          where
+            f (Heads nullableSuccs rSuccs) =
+                choice (map toStartP rSuccs)
+                    <|> whenAlt nullableSuccs succStartP
+
+toStartP :: SectionType -> Parser ()
+toStartP (SectionType kw _ _ _) = void $ keywordP kw
 
 headingP :: Keyword -> HeadingType -> Parser (Maybe Label, Heading)
 headingP kw (HeadingType fmt tt) =
