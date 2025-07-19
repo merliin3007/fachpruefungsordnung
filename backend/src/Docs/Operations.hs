7ac6ab899894628f3e93cac3a8d2862723539a2e
merlin <info@bahn.sh>
2025-07-19 22:23:07 +0200

    add function to obtain tree version with text versions
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/Operations.hs
+++ backend/src/Docs/Operations.hs
@@ -1,5 +1,6 @@
 module Docs.Operations
     ( treeWithTextVersions
+    , treeVersionWithTextVersions
     ) where
 
 import Data.Functor ((<&>))
@@ -9,7 +10,24 @@ import Docs.Text
     , TextElementID
     , TextElementVersion (TextElementVersion)
     )
-import Docs.Tree (Edge (..), Tree (..))
+import Docs.Tree
+    ( Edge (..)
+    , ExistingTreeVersion (ExistingTreeVersion)
+    , Tree (..)
+    , mapMTreeVersionRoot
+    )
+
+treeVersionWithTextVersions
+    :: (Monad m)
+    => (TextElementID -> m ExistingTextVersion)
+    -- ^ (potentially effectful) function for obtaining a text version
+    -> ExistingTreeVersion TextElement
+    -- ^ document structre tree version
+    -> m (ExistingTreeVersion TextElementVersion)
+    -- ^ document structre tree version with concrete text versions
+treeVersionWithTextVersions getTextVersion (ExistingTreeVersion id_ treeVersion) =
+    mapMTreeVersionRoot (treeWithTextVersions getTextVersion) treeVersion
+        <&> ExistingTreeVersion id_
 
 treeWithTextVersions
     :: (Monad m)
