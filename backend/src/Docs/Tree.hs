ea02f2445b15e15aa04f12737e150a50b32ee9b7
merlin <info@bahn.sh>
2025-07-19 20:18:55 +0200

    add function to add text versions to a structure tree
    slight changes to the data structures
    
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/Tree.hs
+++ backend/src/Docs/Tree.hs
@@ -6,12 +6,18 @@ module Docs.Tree
     , ExistingTreeVersion (..)
     , TreeVersionToC
     , TreeVersionFull
+    , treeWithTextVersions
     ) where
 
 import Data.Text (Text)
 import Data.Time (LocalTime)
 import Data.UUID (UUID)
-import Docs.Text (TextElement, TextElementID)
+import Docs.Text
+    ( ExistingTextVersion
+    , TextElement (textElementID)
+    , TextElementID
+    , TextElementVersion (TextElementVersion)
+    )
 import GHC.Int (Int32)
 
 data Tree a
@@ -35,12 +41,12 @@ data ExistingTreeVersion a
         TreeVersionID
         (TreeVersion a)
 
-type TreeVersionToC = TreeVersion TextElementID
+type TreeVersionToC = ExistingTreeVersion TextElement
 
-type TreeVersionFull = TreeVersion TextElement
+type TreeVersionFull = ExistingTreeVersion TextElementVersion
 
 instance Functor Tree where
-    fmap f (Node header edge) = Node header ((f <$>) <$> edge)
+    fmap f (Node header edge) = Node header $ (f <$>) <$> edge
     fmap f (Leaf x) = Leaf $ f x
 
 instance Functor Edge where
@@ -51,5 +57,30 @@ instance Functor TreeVersion where
         TreeVersion
             { treeVersionTimestamp = treeVersionTimestamp treeVersion
             , treeVersionAuthor = treeVersionAuthor treeVersion
-            , treeVersionRoot = treeVersionRoot $ f <$> treeVersion
+            , treeVersionRoot = f <$> treeVersionRoot treeVersion
             }
+
+instance Functor ExistingTreeVersion where
+    fmap f (ExistingTreeVersion id_ treeVersion) =
+        ExistingTreeVersion id_ $ f <$> treeVersion
+
+treeWithTextVersions
+    :: (Monad m)
+    => (TextElementID -> m ExistingTextVersion)
+    -- ^ (potentially effectful) function for obtaining a text version
+    -> Tree TextElement
+    -- ^ document structure tree
+    -> m (Tree TextElementVersion)
+    -- ^ document structure tree with concrete text versions
+treeWithTextVersions getTextVersion = treeWithTextVersions'
+  where
+    treeWithTextVersions' (Leaf textElement) = do
+        existingTextVersion <- getTextVersion $ textElementID textElement
+        return $ Leaf $ TextElementVersion textElement existingTextVersion
+    treeWithTextVersions' (Node metaData edges) = do
+        edgesFull <- mapM mapEdge edges
+        return $ Node metaData edgesFull
+      where
+        mapEdge (Edge label tree) = do
+            treeFull <- treeWithTextVersions' tree
+            return $ Edge label treeFull
