46493011ccd0dab2356e69ae628fdabd1baf0f1d
merlin <info@bahn.sh>
2025-07-20 14:07:01 +0200

    tree text version emplacement functions with maybe
    Add versions of the treeWithTextVersions and
    treeVersionWithTextVersions functions, where the specified getter
    function returns a Maybe.
    
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/Operations.hs
+++ backend/src/Docs/Operations.hs
@@ -1,9 +1,12 @@
 module Docs.Operations
     ( treeWithTextVersions
     , treeVersionWithTextVersions
+    , treeWithMaybeTextVersions
+    , treeVersionWithMaybeTextVersions
     ) where
 
 import Data.Functor ((<&>))
+import Data.Maybe (catMaybes)
 import Docs.Text
     ( ExistingTextVersion
     , TextElement (textElementID)
@@ -14,9 +17,13 @@ import Docs.Tree
     ( Edge (..)
     , ExistingTreeVersion (ExistingTreeVersion)
     , Tree (..)
+    , TreeVersion (treeVersionRoot)
     , mapMTreeVersionRoot
+    , replaceTreeVersionRoot
     )
 
+-- | Takes a tree version and emplaces concrecte text versions.
+-- | The text versions are obtained via the specified getter function.
 treeVersionWithTextVersions
     :: (Monad m)
     => (TextElementID -> m ExistingTextVersion)
@@ -29,6 +36,8 @@ treeVersionWithTextVersions getTextVersion (ExistingTreeVersion id_ treeVersion)
     mapMTreeVersionRoot (treeWithTextVersions getTextVersion) treeVersion
         <&> ExistingTreeVersion id_
 
+-- | Takes a tree and emplaces concrete text versions.
+-- | The text versions are obtained via the specified getter function.
 treeWithTextVersions
     :: (Monad m)
     => (TextElementID -> m ExistingTextVersion)
@@ -47,3 +56,48 @@ treeWithTextVersions getTextVersion = treeWithTextVersions'
       where
         mapEdge (Edge label tree) =
             treeWithTextVersions' tree <&> Edge label
+
+-- | Takes a tree version and emplaces concrecte text versions.
+-- | The text versions are obtained via the specified getter function.
+-- | This function may return 'Nothing'. In such a case, the corresponding Leaf is
+-- | missing in the tree of the resulting tree version.
+-- | If the root is a leaf and is missing, the result will be 'Nothing'.
+treeVersionWithMaybeTextVersions
+    :: (Monad m)
+    => (TextElementID -> m (Maybe ExistingTextVersion))
+    -- ^ (potentially effectful) function for obtaining a text version
+    -> ExistingTreeVersion TextElement
+    -- ^ document structre tree version
+    -> m (Maybe (ExistingTreeVersion TextElementVersion))
+    -- ^ document structre tree version with concrete text versions
+treeVersionWithMaybeTextVersions
+    getTextVersion
+    (ExistingTreeVersion id_ treeVersion) = do
+        let oldRoot = treeVersionRoot treeVersion
+        newRoot <- treeWithMaybeTextVersions getTextVersion oldRoot
+        let newTreeVersion = replaceTreeVersionRoot <$> newRoot <*> pure treeVersion
+        return $ ExistingTreeVersion id_ <$> newTreeVersion
+
+-- | Takes a tree and emplaces concrete text versions.
+-- | The text versions are obtained via the specified getter function.
+-- | This function may return 'Nothing'. In such a case, the corresponding Leaf is
+-- | missing in the resulting tree version.
+-- | If the root is a leaf and is missing, the result will be 'Nothing'.
+treeWithMaybeTextVersions
+    :: (Monad m)
+    => (TextElementID -> m (Maybe ExistingTextVersion))
+    -- ^ (potentially effectful) function for obtaining a text version
+    -> Tree TextElement
+    -- ^ document structure tree
+    -> m (Maybe (Tree TextElementVersion))
+    -- ^ document structure tree with concrete text versions
+treeWithMaybeTextVersions getTextVersion = treeWithTextVersions'
+  where
+    treeWithTextVersions' (Leaf textElement) =
+        getTextVersion (textElementID textElement)
+            <&> (Leaf . TextElementVersion textElement <$>)
+    treeWithTextVersions' (Node metaData edges) =
+        mapM mapEdge edges <&> Just . Node metaData . catMaybes
+      where
+        mapEdge (Edge label tree) =
+            treeWithTextVersions' tree <&> (Edge label <$>)
