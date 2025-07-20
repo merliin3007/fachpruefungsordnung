8cd0c68cb14178c1296c15817ab91b2385d7ef3e
merlin <info@bahn.sh>
2025-07-20 21:36:31 +0200

    expose createTextVersion in internal api
    Also, the repository now has its own module.
    
    Co-authored-by: Finn Evers <dev@bahn.sh>

---
+++ backend/src/Docs/Repository.hs
@@ -0,0 +1,12 @@
+module Docs.Repository (Repository (..)) where
+
+import Data.Text (Text)
+import Docs.TextElement (TextElement, TextElementID)
+import Docs.TextRevision (TextRevision, TextRevisionID)
+import Docs.Util (UserID)
+
+data (Monad m) => Repository m = Repository
+    { getLatestTextRevisionID :: TextElementID -> m (Maybe TextRevisionID)
+    , createTextRevision :: TextElementID -> UserID -> Text -> m TextRevision
+    , getTextElement :: TextElementID -> m TextElement
+    }
