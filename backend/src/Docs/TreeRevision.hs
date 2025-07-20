11ea83c5cff257fc46c1b800642d188905b36918
merlin <info@bahn.sh>
2025-07-20 19:10:38 +0200

    refactor Operations.hs away
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/TreeRevision.hs
+++ backend/src/Docs/TreeRevision.hs
@@ -5,11 +5,17 @@ module Docs.TreeRevision
     , mapRoot
     , mapMRoot
     , replaceRoot
+    , withTextRevisions
+    , withMaybeTextRevisions
     ) where
 
+import Data.Functor ((<&>))
 import Data.Time (UTCTime)
 import Data.UUID (UUID)
+import Docs.TextElement (TextElement, TextElementID)
+import Docs.TextRevision (TextElementRevision, TextRevision)
 import Docs.Tree (Tree)
+import qualified Docs.Tree as Tree
 import GHC.Int (Int32)
 
 newtype TreeRevisionID = TreeRevisionID
@@ -58,3 +64,36 @@ instance Functor TreeRevision where
 instance Functor ExistingTreeRevision where
     fmap f (ExistingTreeRevision id_ treeVersion) =
         ExistingTreeRevision id_ $ f <$> treeVersion
+
+-- | Takes a tree revision and emplaces concrecte text revisions.
+-- | The text revisions are obtained via the specified getter function.
+withTextRevisions
+    :: (Monad m)
+    => (TextElementID -> m TextRevision)
+    -- ^ (potentially effectful) function for obtaining a text revision
+    -> ExistingTreeRevision TextElement
+    -- ^ document structre tree revision
+    -> m (ExistingTreeRevision TextElementRevision)
+    -- ^ document structre tree revision with concrete text revision
+withTextRevisions getTextRevision (ExistingTreeRevision id_ treeVersion) =
+    mapMRoot (Tree.withTextRevisions getTextRevision) treeVersion
+        <&> ExistingTreeRevision id_
+
+-- | Takes a tree revision and emplaces concrecte text revisions.
+-- | The text revisions are obtained via the specified getter function.
+-- | This function may return 'Nothing'. In such a case, the corresponding Leaf is
+-- | missing in the tree of the resulting tree revision.
+-- | If the root is a leaf and is missing, the result will be 'Nothing'.
+withMaybeTextRevisions
+    :: (Monad m)
+    => (TextElementID -> m (Maybe TextRevision))
+    -- ^ (potentially effectful) function for obtaining a text revision
+    -> ExistingTreeRevision TextElement
+    -- ^ document structre tree revision
+    -> m (Maybe (ExistingTreeRevision TextElementRevision))
+    -- ^ document structre tree revision with concrete text revision
+withMaybeTextRevisions getTextRevision (ExistingTreeRevision id_ treeVersion) = do
+    let oldRoot = root treeVersion
+    newRoot <- Tree.withMaybeTextRevisions getTextRevision oldRoot
+    let newTreeVersion = replaceRoot <$> newRoot <*> pure treeVersion
+    return $ ExistingTreeRevision id_ <$> newTreeVersion
