fdacc7f06f4a9fa0dc323aedb383377a5781f60f
merlin <info@bahn.sh>
2025-07-22 18:23:49 +0200

    implement `putTree` hasql transaction
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/Hasql/TreeEdge.hs
+++ backend/src/Docs/Hasql/TreeEdge.hs
@@ -1,14 +1,26 @@
-module Docs.Hasql.TreeEdge (TreeEdge (..)) where
+module Docs.Hasql.TreeEdge
+    ( TreeEdge (..)
+    , TreeEdgeChildRef (..)
+    ) where
 
 import Data.Text (Text)
 import GHC.Int (Int32)
 
 import Docs.TextElement (TextElementID)
-import DocumentManagement.Hash (Hash)
+import DocumentManagement.Hash (Hash, Hashable (..))
+
+data TreeEdgeChildRef
+    = TreeEdgeToTextElement TextElementID
+    | TreeEdgeToNode Hash
+
+instance Hashable TreeEdgeChildRef where
+    updateHash ctx (TreeEdgeToTextElement elementID) =
+        updateHash ctx elementID
+    updateHash ctx (TreeEdgeToNode nodeHash) = updateHash ctx nodeHash
 
 data TreeEdge = TreeEdge
     { parentHash :: Hash
     , position :: Int32
     , title :: Text
-    , child :: Either TextElementID Hash
+    , child :: TreeEdgeChildRef
     }
