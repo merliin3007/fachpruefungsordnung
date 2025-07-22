31044f27a8678d866e87be88515198c8c6ddc5f2
merlin <info@bahn.sh>
2025-07-22 16:24:05 +0200

    enforce `TextRevision`s root is not a leaf
    See https://github.com/fachpruefungsordnung/fachpruefungsordnung/pull/260#issuecomment-3101588137
    
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/Tree.hs
+++ backend/src/Docs/Tree.hs
@@ -1,6 +1,7 @@
 module Docs.Tree
     ( Tree (..)
     , Edge (..)
+    , Node (..)
     , withTextRevisions
     , withMaybeTextRevisions
     ) where
@@ -16,14 +17,19 @@ import Docs.TextRevision
     , TextRevision
     )
 
+data Node a = Node Text [Edge a]
+
 data Tree a
-    = Node Text [Edge a]
+    = Tree (Node a)
     | Leaf a
 
 data Edge a = Edge Text (Tree a)
 
-instance Functor Tree where
+instance Functor Node where
     fmap f (Node header edge) = Node header $ (f <$>) <$> edge
+
+instance Functor Tree where
+    fmap f (Tree node) = Tree $ f <$> node
     fmap f (Leaf x) = Leaf $ f x
 
 instance Functor Edge where
@@ -35,41 +41,38 @@ withTextRevisions
     :: (Monad m)
     => (TextElementID -> m TextRevision)
     -- ^ (potentially effectful) function for obtaining a text revision
-    -> Tree TextElement
+    -> Node TextElement
     -- ^ document structure tree
-    -> m (Tree TextElementRevision)
+    -> m (Node TextElementRevision)
     -- ^ document structure tree with concrete text revisions
 withTextRevisions getTextRevision = withTextRevisions'
   where
-    withTextRevisions' (Leaf textElement) =
+    withTextRevisions' (Node metaData edges) = mapM mapEdge edges <&> Node metaData
+    treeWithTextRevisions (Tree node) = withTextRevisions' node <&> Tree
+    treeWithTextRevisions (Leaf textElement) =
         getTextRevision (TextElement.identifier textElement)
             <&> Leaf . TextElementRevision textElement
-    withTextRevisions' (Node metaData edges) =
-        mapM mapEdge edges <&> Node metaData
-      where
-        mapEdge (Edge label tree) =
-            withTextRevisions' tree <&> Edge label
+    mapEdge (Edge label tree) = treeWithTextRevisions tree <&> Edge label
 
 -- | Takes a tree and emplaces concrete text revisions.
 -- | The text revisions are obtained via the specified getter function.
 -- | This function may return 'Nothing'. In such a case, the corresponding Leaf is
 -- | missing in the resulting tree revision.
--- | If the root is a leaf and is missing, the result will be 'Nothing'.
 withMaybeTextRevisions
     :: (Monad m)
     => (TextElementID -> m (Maybe TextRevision))
     -- ^ (potentially effectful) function for obtaining a text revision
-    -> Tree TextElement
+    -> Node TextElement
     -- ^ document structure tree
-    -> m (Maybe (Tree TextElementRevision))
+    -> m (Node TextElementRevision)
     -- ^ document structure tree with concrete text revision
 withMaybeTextRevisions getTextRevision = withMaybeTextRevisions'
   where
-    withMaybeTextRevisions' (Leaf textElement) =
+    withMaybeTextRevisions' (Node metaData edges) =
+        mapM mapEdge edges <&> Node metaData . catMaybes
+    treeWithMaybeTextRevisions (Tree node) =
+        withMaybeTextRevisions' node <&> Just . Tree
+    treeWithMaybeTextRevisions (Leaf textElement) =
         getTextRevision (TextElement.identifier textElement)
             <&> (Leaf . TextElementRevision textElement <$>)
-    withMaybeTextRevisions' (Node metaData edges) =
-        mapM mapEdge edges <&> Just . Node metaData . catMaybes
-      where
-        mapEdge (Edge label tree) =
-            withMaybeTextRevisions' tree <&> (Edge label <$>)
+    mapEdge (Edge label tree) = treeWithMaybeTextRevisions tree <&> (Edge label <$>)
