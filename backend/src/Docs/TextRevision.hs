671b8791a3d16b99baf5d728102a9b1d2d464e4f
Merlin <merliin3007@gmail.com>
2025-07-27 14:57:33 +0200

    dedicated `ConflictStatus` instead of `Either`
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/TextRevision.hs
+++ backend/src/Docs/TextRevision.hs
@@ -7,7 +7,7 @@ module Docs.TextRevision
     , TextRevision (..)
     , TextRevisionHeader (..)
     , TextElementRevision (..)
-    , TextRevisionConflict (..)
+    , ConflictStatus (..)
     , TextRevisionHistory (..)
     , NewTextRevision (..)
     , TextRevisionRef (..)
@@ -236,10 +236,10 @@ data NewTextRevision = NewTextRevision
     }
 
 -- | A conflict with another text revision.
-newtype TextRevisionConflict
-    = TextRevisionConflict TextRevisionID -- todo: maybe not id but whole TextRevision?
+data ConflictStatus
+    = Conflict TextRevisionID -- todo: maybe not id but whole TextRevision?
+    | NoConflict TextRevision
 
--- | Creates a new text revision.
 -- | Returns a conflict, if the parent revision is not the latest revision.
 -- | If the text element does not have any revision, but a parent revision is set,
 -- | it is ignored.
@@ -253,16 +253,16 @@ newTextRevision
     -- ^ the id of the user who intends to create the new revision
     -> NewTextRevision
     -- ^ all data needed to create a new text revision
-    -> m (Either TextRevisionConflict TextRevision)
+    -> m ConflictStatus
     -- ^ either the newly created text revision or a conflict
 newTextRevision getLatestRevisionID createRevision userID newRevision = do
     latestRevisionID <- getLatestRevisionID $ newTextRevisionElement newRevision
     let parentRevisionID = newTextRevisionParent newRevision
     case latestRevisionID of
-        Nothing -> createRevision' <&> Right
+        Nothing -> createRevision' <&> NoConflict
         Just latest
-            | latestRevisionID == parentRevisionID -> createRevision' <&> Right
-            | otherwise -> return $ Left $ TextRevisionConflict latest
+            | latestRevisionID == parentRevisionID -> createRevision' <&> NoConflict
+            | otherwise -> return $ Conflict latest
   where
     createRevision' =
         createRevision
