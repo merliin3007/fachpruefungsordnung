1e52c57ce85fabb240ffa992b68a0074a30fa42d
Merlin <merliin3007@gmail.com>
2025-07-27 03:00:30 +0200

    WIP: `ToJSON`, `FromJSON` and `ToSchema`
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/Docs/Document.hs
+++ backend/src/Docs/Document.hs
@@ -1,18 +1,64 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Docs.Document
     ( DocumentID (..)
     , Document (..)
     ) where
 
+import Data.Proxy (Proxy (..))
 import Data.Text (Text)
+
+import GHC.Generics (Generic)
 import GHC.Int (Int32)
 
+import Web.HttpApiData (FromHttpApiData (..))
+
+import Control.Lens ((&), (?~))
+import Data.Aeson (FromJSON (..), ToJSON (..))
+import Data.OpenApi
+    ( OpenApiType (..)
+    , ToParamSchema (..)
+    , ToSchema (..)
+    , exclusiveMinimum
+    , minimum_
+    , type_
+    )
+
+-- | ID for a document
 newtype DocumentID = DocumentID
     { unDocumentID :: Int32
     }
-    deriving (Eq)
+    deriving (Eq, Show)
 
+instance ToJSON DocumentID where
+    toJSON = toJSON . unDocumentID
+
+instance FromJSON DocumentID where
+    parseJSON = fmap DocumentID . parseJSON
+
+instance ToSchema DocumentID where
+    declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy Int32)
+
+instance ToParamSchema DocumentID where
+    toParamSchema _ =
+        mempty
+            & type_ ?~ OpenApiInteger
+            & minimum_ ?~ 0
+            & exclusiveMinimum ?~ False
+
+instance FromHttpApiData DocumentID where
+    parseUrlPiece = (DocumentID <$>) . parseUrlPiece
+
+-- | Document metadata
 data Document = Document
     { identifier :: DocumentID
     , name :: Text
     , group :: Int32
     }
+    deriving (Eq, Show, Generic)
+
+instance ToJSON Document
+
+instance FromJSON Document
+
+instance ToSchema Document
