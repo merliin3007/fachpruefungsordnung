5a488cae1025e756acbdac87ad5aa566e0d15066
stu236751 <stu236751@mail.uni-kiel.de>
2025-08-01 18:34:26 +0200

    fixed some issues from the merge

--- frontend/src/FPO/Data/Request.purs
+++ frontend/src/FPO/Data/Request.purs
@@ -37,7 +37,7 @@ import FPO.Dto.UserDto (FullUserDto, Role, UserID)
 >>>>>>> main -}
 import FPO.Dto.CreateDocumentDto (DocumentCreateDto, NewDocumentCreateDto)
 import FPO.Dto.DocumentDto (DocumentHeader, DocumentHeaderPlusPermission, DocumentID, NewDocumentHeader)
-import FPO.Dto.GroupDto (GroupCreate, GroupDto, GroupID, GroupOverview, Role, UserID)
+import FPO.Dto.GroupDto (GroupCreate, GroupDto, GroupID, GroupOverview)
 import FPO.Dto.UserDto (FullUserDto, Role, UserID)
 import Foreign (renderForeignError)
 import Web.DOM.Document (Document)
