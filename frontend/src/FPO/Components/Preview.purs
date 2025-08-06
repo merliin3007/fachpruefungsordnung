4fcf666a236f900bc6917ee8b5f19f2367870a41
Nick Schmahl <nick.schmahl@wps.de>
2025-08-06 23:03:08 +0200

    After closing and opening the preview, load the injected html immediately

--- frontend/src/FPO/Components/Preview.purs
+++ frontend/src/FPO/Components/Preview.purs
@@ -29,7 +29,7 @@ type Input = { renderedHtml :: Maybe String }
 
 preview :: forall m. MonadAff m => H.Component Query Input Output m
 preview = H.mkComponent
-  { initialState: const initialState
+  { initialState
   , render
   , eval: H.mkEval H.defaultEval
       { handleAction = handleAction
@@ -38,10 +38,9 @@ preview = H.mkComponent
       }
   }
   where
-  initialState :: State
-  initialState =
-    { renderedHtml: Nothing
-    }
+  initialState :: Input -> State
+  initialState { renderedHtml } =
+    { renderedHtml: renderedHtml }
 
   render :: State -> H.ComponentHTML Action Slots m
   render _ =
@@ -50,7 +49,20 @@ preview = H.mkComponent
   handleAction :: MonadAff m => Action -> H.HalogenM State Action Slots Unit m Unit
   handleAction = case _ of
     Initialize -> do
-      pure unit
+      -- On initialization, we check if there is already rendered HTML in the state
+      -- and inject it into the HTML element if it exists.
+      -- This is useful when we open the preview component again after it has been rendered before.
+      state <- H.get
+      let renderedHtml = state.renderedHtml
+      htmlElementRef <- getHTMLElementRef (RefLabel "injectHtml")
+      case htmlElementRef of
+        Just ref -> do
+          case renderedHtml of
+            Just htmlContent -> do
+              H.liftEffect $ setInnerHtml ref htmlContent
+              pure unit
+            Nothing -> pure unit
+        Nothing -> pure unit
 
     Receive { renderedHtml } -> do
       htmlElementRef <- getHTMLElementRef (RefLabel "injectHtml")
