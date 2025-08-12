d88e6373fc0f97abf4a184bd57e1bff3d303d9e7
lassezuengel <lassezuengel@gmail.com>
2025-08-12 19:51:20 +0200

    Add support for section renaming in TOC
    Also disallows dragging sections into themselves.

--- frontend/src/FPO/Util.purs
+++ frontend/src/FPO/Util.purs
@@ -3,7 +3,19 @@ module FPO.Util where
 import Prelude
 
 import Control.Alternative (guard)
+import Data.Array (length, take)
 
 -- | Prepends an element to an array if the condition is true.
 prependIf :: forall a. Boolean -> a -> Array a -> Array a
 prependIf cond x xs = (guard cond $> x) <> xs
+
+isPrefixOf :: forall a. Eq a => Array a -> Array a -> Boolean
+isPrefixOf prefix arr =
+  take (length prefix) arr == prefix
+
+isRealPrefixOf :: forall a. Eq a => Array a -> Array a -> Boolean
+isRealPrefixOf prefix arr =
+  let
+    len = length prefix
+  in
+    len > 0 && isPrefixOf prefix arr && length arr > len
