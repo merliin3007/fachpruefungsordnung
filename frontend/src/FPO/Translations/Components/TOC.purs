31012e49f933275fecf637a929dc1973cf9a2c40
lassezuengel <lassezuengel@gmail.com>
2025-08-13 18:06:04 +0200

    Prettier titles and deletion modal
    Labels of paragraphs are omitted in the TOC, and
    deletion text in modals is improved (and i8nized).

--- frontend/src/FPO/Translations/Components/TOC.purs
+++ frontend/src/FPO/Translations/Components/TOC.purs
@@ -5,15 +5,21 @@ import Simple.I18n.Translation (Translation, fromRecord)
 
 type TocLabels =
   ( "toc_end_dropzone"
+      ::: "toc_paragraph"
+      ::: "toc_section"
       ::: SNil
   )
 
 enTOC :: Translation TocLabels
 enTOC = fromRecord
   { toc_end_dropzone: "Drop here to add to end of section"
+  , toc_paragraph: "Paragraph"
+  , toc_section: "Section"
   }
 
 deTOC :: Translation TocLabels
 deTOC = fromRecord
   { toc_end_dropzone: "Am Ende einfügen"
+  , toc_paragraph: "den Paragraphen"
+  , toc_section: "den Abschnitt"
   }
